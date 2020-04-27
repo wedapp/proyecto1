1328812c683c387049ecad9c05a4d75a44452af6
Daniel Alves <daniel@wedapp.io>
2020-04-26 22:55:29 -0400

    generada librería de frases

---
+++ proyecto1/Library/quotes.swift
@@ -0,0 +1,29 @@
+//
+//  File.swift
+//  proyecto1
+//
+//  Created by Weda Alves on 26-04-20.
+//  Copyright © 2020 wedapp. All rights reserved.
+//
+
+import Foundation
+
+var loveQuotes = []
+
+var euphoriaQuotes = []
+
+var joyQuotes = []
+
+var confidenceQuotes = []
+
+var motivationQuotes = ["Mi trabajo no es caer bien a la gente. Mi trabajo es hacerles mejores. Steve Jobs", "Tu tiempo es limitado, de modo que no lo malgastes viviendo la vida de alguien distinto. No quedes atrapado en el dogma, que es vivir como otros piensan que deberías vivir. No dejes que los ruidos de las opiniones de los demás acallen tu propia voz interior. Y, lo que es más importante, ten el coraje para hacer lo que te dice tu corazón y tu intuición. Steve Jobs", "Si vives cada día de tu vida como si fuera el último, algún día realmente tendrás razón. Steve Jobs"]
+
+var sadnessQuotes = []
+
+var angerQuotes = []
+
+var lonelinessQuotes = []
+
+var fearQuotes = []
+
+var impatienceQuotes = []
