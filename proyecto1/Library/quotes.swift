c98218d58f3d2c82371f640c9f7f88bd9d403100
Daniel Alves <daniel@wedapp.io>
2020-04-28 22:50:44 -0400

    implementación stack views

--- proyecto1/Library/quotes.swift
+++ proyecto1/Library/quotes.swift
@@ -10,20 +10,20 @@ import Foundation
 
 var loveQuotes = ["Hay amores tan bellos que justifican todas las locuras que hacen cometer. Plutarco", "Cierto que en el mundo de los hombres nada hay necesario, excepto el amor. Goethe", "Hay un camino seguro para llegar a todo corazón: el amor. Concepción Arenal", "El amor sólo descansa cuando muere. Un amor vivo es un amor en conflicto. Paulo Coelho", "No ser amados es una simple desventura; la verdadera desgracia es no amar. Albert Camus", "Ni la ausencia ni el tiempo son nada cuando se ama. Alfred de Musset", "En asuntos de amor, los locos son los que tienen más experiencia. De amor no preguntes nunca a los cuerdos; los cuerdos aman cuerdamente, que es como no haber amado nunca. Jacinto Benavente", "Qué lástima que pase el tiempo ¿verdad?, qué lástima que nos muramos y que nos hagamos viejos y que las cosas buenas se vayan alejando de nosotros al galope. Roberto Bolaño", "El que ha conocido solo a su mujer y la ha amado, sabe más de mujeres que el que ha conocido mil. Leon Tolstoi", "Hay que saber que no existe país sobre la tierra donde el amor no haya convertido a los amantes en poetas. Voltaire", "El amor es el anhelo de salir de uno mismo. Charles Baudelaire", "Un beso legal nunca vale tanto como un beso robado. Guy de Maupassant", "Las cartas de amor se empiezan sin saber lo que se va a decir y se terminan sin saber lo que se ha dicho. Jean-Jacques Rousseau", "El amor es tan importante como la comida. Pero no alimenta. García Márquez", "Donde no hay amor, poned amor y encontraréis amor. San Juan de la Cruz", "Te amo por encima de todo aquello que no podemos ver, por encima de lo que no podemos conocer. Federico Moccia", "Es tan corto el amor y es tan largo el olvido… Pablo Neruda", "Sabes que estas enamorado cuando no quieres acostarte porque la realidad es por fin mejor que tus sueños. Dr. Seuss", "Ama un solo día y el mundo habrá cambiado. Robert Browning", "El que puede prescindir del ser amado puede prescindir de todo. André Maurois"]
 
-var euphoriaQuotes = []
+var euphoriaQuotes = ["Daniel"]
 
-var joyQuotes = []
+var joyQuotes = ["Daniel"]
 
-var confidenceQuotes = []
+var confidenceQuotes = ["Daniel"]
 
 var motivationQuotes = ["Mi trabajo no es caer bien a la gente. Mi trabajo es hacerles mejores. Steve Jobs", "Tu tiempo es limitado, de modo que no lo malgastes viviendo la vida de alguien distinto. No quedes atrapado en el dogma, que es vivir como otros piensan que deberías vivir. No dejes que los ruidos de las opiniones de los demás acallen tu propia voz interior. Y, lo que es más importante, ten el coraje para hacer lo que te dice tu corazón y tu intuición. Steve Jobs", "Si vives cada día de tu vida como si fuera el último, algún día realmente tendrás razón. Steve Jobs"]
 
-var sadnessQuotes = []
+var sadnessQuotes = ["Daniel"]
 
-var angerQuotes = []
+var angerQuotes = ["Daniel"]
 
-var lonelinessQuotes = []
+var lonelinessQuotes = ["Daniel"]
 
-var fearQuotes = []
+var fearQuotes = ["Daniel"]
 
-var impatienceQuotes = []
+var impatienceQuotes = ["Daniel"]
