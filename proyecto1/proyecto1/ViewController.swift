4c3691e99005c691d5e9e40065ef9522f733206c
Daniel Alves <daniel@wedapp.io>
2020-05-01 23:30:51 -0400

    segues finalizados; paso de pantalla 1 a 2 finalizado; texto paso de pantalla 2 a 3 finalizado; color paso pantalla 2 a 3 no funciona

--- proyecto1/proyecto1/ViewController.swift
+++ proyecto1/proyecto1/ViewController.swift
@@ -12,26 +12,92 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var colorQuestion: UILabel!
     @IBOutlet weak var wedappLabel: UILabel!
+    @IBOutlet weak var myButtonRed: UIButton!
+    @IBOutlet weak var myButtonOrange: UIButton!
+    @IBOutlet weak var myButtonYellow: UIButton!
+    @IBOutlet weak var myButtonGreen: UIButton!
+    @IBOutlet weak var myButtonBlue: UIButton!
+    @IBOutlet weak var myButtonLilac: UIButton!
+    @IBOutlet weak var myButtonPurple: UIButton!
     
     override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
-    
+        
+        myButtonRed.backgroundColor = .red
+        myButtonRed.setTitle("Rojo", for: .normal)
+        myButtonRed.setTitleColor(.red, for: .normal)
+        
+        myButtonOrange.backgroundColor = .orange
+        myButtonOrange.setTitle("Narajo", for: .normal)
+        myButtonOrange.setTitleColor(.orange, for: .normal)
+        
+        myButtonYellow.backgroundColor = .yellow
+        myButtonYellow.setTitle("Amarillo", for: .normal)
+        myButtonYellow.setTitleColor(.yellow, for: .normal)
+        
+        myButtonGreen.backgroundColor = .green
+        myButtonGreen.setTitle("Verde", for: .normal)
+        myButtonGreen.setTitleColor(.green, for: .normal)
+        
+        myButtonBlue.backgroundColor = .blue
+        myButtonBlue.setTitle("Azul", for: .normal)
+        myButtonBlue.setTitleColor(.blue, for: .normal)
+        
+        myButtonLilac.backgroundColor = .systemPink
+        myButtonLilac.setTitle("Rosa", for: .normal)
+        myButtonLilac.setTitleColor(.systemPink, for: .normal)
+        
+        myButtonPurple.backgroundColor = .purple
+        myButtonPurple.setTitle("Morado", for: .normal)
+        myButtonPurple.setTitleColor(.purple, for: .normal	)
+        
     }
     // Botones Color
-    @IBAction func lilaButtonAction(_ sender: Any) {
+    @IBAction func buttonRed(_ sender: UIButton) {
+        // Código que al pulsar el botón rojo pinte la pantalla siguiente de rojo
+        let colorDeFondoColores = [myButtonRed.backgroundColor]
+        // Código que al pulsar el botón rojo nos mande para la pantalla de palabras
+        performSegue(withIdentifier: "VCWords", sender: colorDeFondoColores)
     }
-    @IBAction func orangeButtonAction(_ sender: Any) {
+    @IBAction func buttonOrange(_ sender: UIButton) {
+        let colorDeFondoColores = [myButtonOrange.backgroundColor]
+        performSegue(withIdentifier: "VCWords", sender: colorDeFondoColores)
     }
-    @IBAction func purpleButtonAction(_ sender: Any) {
+    @IBAction func buttonYellow(_ sender: UIButton) {
+        let colorDeFondoColores = [myButtonYellow.backgroundColor]
+        performSegue(withIdentifier: "VCWords", sender: colorDeFondoColores)
     }
-    @IBAction func blueButtonAction(_ sender: Any) {
+    @IBAction func buttonGreen(_ sender: UIButton) {
+        let colorDeFondoColores = [myButtonGreen.backgroundColor]
+        performSegue(withIdentifier: "VCWords", sender: colorDeFondoColores)
     }
-    @IBAction func yellowButtonAction(_ sender: Any) {
+    @IBAction func buttonBlue(_ sender: UIButton) {
+        let colorDeFondoColores = [myButtonBlue.backgroundColor]
+        performSegue(withIdentifier: "VCWords", sender: colorDeFondoColores)
     }
-    @IBAction func redButtonAction(_ sender: Any) {
+    @IBAction func buttonLilac(_ sender: UIButton) {
+        let colorDeFondoColores = [myButtonLilac.backgroundColor]
+        performSegue(withIdentifier: "VCWords", sender: colorDeFondoColores)
     }
-    @IBAction func greenButtonAction(_ sender: Any) {
+    @IBAction func buttonPurple(_ sender: UIButton) {
+        let colorDeFondoColores = [myButtonPurple.backgroundColor]
+        performSegue(withIdentifier: "VCWords", sender: colorDeFondoColores)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "VCWords" || segue.identifier == "VCMessage2" {
+            if let destino = segue.destination as? ViewControllerWords {
+                if let colorDeFondoColores = sender as? Array<Any> {
+                    destino.colorDeFondo = colorDeFondoColores[0] as? UIColor
+                }
+            }
+            
+            if let destino = segue.destination as? ViewControllerMessage {
+            if let colorDeFondoColores = sender as? Array<Any> {
+                destino.colorDeFondo = colorDeFondoColores[0] as? UIColor
+                }
+            }
+    
+        }
     }
 }
-
